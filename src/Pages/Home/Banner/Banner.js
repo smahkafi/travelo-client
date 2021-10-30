145134331b100a9c1c0d315a76ed3cd16ecbd9f5
smahkafi <sakafi.m@gmail.com>
2021-10-30 23:18:32 +0600

    banner img resize

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,45 +1,85 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import Banner1 from '../../../xbg_2.jpg.pagespeed.ic.st0QgdOL7t.webp'
-// import Banner1 from '../../../xbg_2.jpg.pagespeed.ic.st0QgdOL7t.webp'
-// import Banner1 from '../../../xbg_2.jpg.pagespeed.ic.st0QgdOL7t.webp'
+import sajek from '../../../images/Carusel/sajek.jpg'
+import rangamati from '../../../images/Carusel/Rangamati2.jpg'
+import Cox from '../../../images/Carusel/Coxs-bazar-banner.jpg'
+import saintMartin from '../../../images/Carusel/saintmartin.jpg'
+import sundorban from '../../../images/Carusel/sundorban.jpg'
+import kuakata from '../../../images/Carusel/kuakata.jpg'
+import './Banner.css'
 
 const Banner = () => {
     return (
             <Carousel>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100 h-75 banner"
-                        src={Banner1}
+                        className="d-block w-100 carosel-img"
+                        src={sajek}
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h1>Sajek Vally</h1>
+                        <p>Sajek Valley is one of the popular tourist spots in Bangladesh situated among the hills of the Kasalong range of mountains in Sajek union, Baghaichhari Upazila in Rangamati District. </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100 banner"
-                        src={Banner1}
+                        className="d-block w-100 carosel-img"
+                        src={rangamati}
                         alt="Second slide"
                     />
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h1>Second slide label</h1>
+                        <p>Kaptai Lake is the largest lake in Bangladesh. It is located in the Kaptai Upazila under Rangamati District of Chittagong Division.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-100 banner"
-                        src={Banner1}
+                        className="d-block w-100 carosel-img"
+                        src={Cox}
                         alt="Third slide"
                     />
 
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h1 className="">Cox's Bazar Beach</h1>
+                        <h6 className="">Cox's Bazar Beach, located at Cox's Bazar, Bangladesh, is the longest natural sea beach in the world running 150 kilometres, and 3rd longest beach after Praia do Cassino of Brazil and Ninety Mile Beach of Australia.</h6>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 carosel-img"
+                        src={saintMartin}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h1>St. Martin's Island</h1>
+                        <p>St. Martin's Island is a small island in the northeastern part of the Bay of Bengal, about 9 km south of the tip of the Cox's Bazar-Teknaf peninsula, and forming the southernmost part of Bangladesh</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 carosel-img"
+                        src={sundorban}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h1>Sundarban</h1>
+                        <p>Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 carosel-img"
+                        src={kuakata}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h1>Kuakata Sea Beach</h1>
+                        <p>Kuakata Beach is a beach situated in Kuakata, Patuakhali District, Bangladesh.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
