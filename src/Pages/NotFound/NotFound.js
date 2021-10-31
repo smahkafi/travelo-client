baff2bb34a55764838f09e35ac80e921e8978d88
smahkafi <sakafi.m@gmail.com>
2021-10-31 22:12:56 +0600

    add 404 photo

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import error from '../../images/404/E404.png'
 
 const NotFound = () => {
     return (
         <div>
-            <h1>Page Not Found</h1>
+            <img src={error} className="img-fluid"  alt="" />
         </div>
     );
 };
